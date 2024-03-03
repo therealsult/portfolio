b90a6930ab0611c4c71bf4e2cfca6909219b2006
therealsult <113542978+therealsult@users.noreply.github.com>
2024-03-03 12:19:35 -0500

    fixed CMS
    added .gitignore

--- admin/add_project.php
+++ admin/add_project.php
@@ -1,14 +1,21 @@
 <?php
 require_once('../includes/connect.php');
-$query = "INSERT INTO projects (title,description,image_url) VALUES (?,?,?)";
+
+//move_uploaded_file etc FIRST, as we need the new name
+//save the name in $filename variable
+
+
+
+$query = "INSERT INTO projects (title,project_description,published_date) VALUES (?,?,?)";
 
 $stmt = $connection->prepare($query);
 $stmt->bindParam(1, $_POST['title'], PDO::PARAM_STR);
-$stmt->bindParam(2, $_POST['desc'], PDO::PARAM_STR);
-$stmt->bindParam(3, $_POST['thumb'], PDO::PARAM_STR);
+$stmt->bindParam(2, $_POST['project_description'], PDO::PARAM_STR);
+$stmt->bindParam(3, $_POST['published_date'], PDO::PARAM_STR);
+// $stmt->bindParam(4, $filename, PDO::PARAM_STR);
 
 $stmt->execute();
 $last_id = $connection->lastInsertId();
 $stmt = null;
-header('Location: project_list.php');
+header('Location: branding.php');
 ?>
